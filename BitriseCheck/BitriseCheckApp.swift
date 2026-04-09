adf2f7828e1660f1c9f5f0244742cf3147aa9f0e
arvinthmani <arvinthmani91@gmail.com>
2026-04-09 15:52:18 +0530

    Initial Commit

---
+++ BitriseCheck/BitriseCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitriseCheckApp.swift
+//  BitriseCheck
+//
+//  Created by Arvinth on 09/04/26.
+//
+
+import SwiftUI
+
+@main
+struct BitriseCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitriseCheck/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BitriseCheck
+//
+//  Created by Arvinth on 09/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Bitrise!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
